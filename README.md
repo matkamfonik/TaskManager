e3fbd25e3f3a6500c5bfcee6c051ffa9b6078dcd
matkamfonik <mat.kamfonik@gmail.com>
2022-11-24 23:21:08 +0100

    added reading and souting Database

---
+++ README.md
@@ -0,0 +1,21 @@
+# TASK
+Console program for managing Tasks
+
+## INTRODUCTION
+**Tasks** is simple JAVA program to manage Your... _wait for it_... tasks!.
+
+## FUNCTIONALITY
+Using console You can list, add or delete your added tasks.
+
+**Tasks** will ask for 
+
++ _task description_, 
++ _due date_, 
++ if _task is important_,
+
+and store data in legible table.
+
+## HOW TO USE IT
+Just run Your favourite console and type 'Tasks'.
+
+**Thank You for using TASKS**
--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,7 +1,23 @@
 package pl.coderslab;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class TaskManager {
     public static void main(String[] args) {
+        String databaseName = "tasks.csv";
+        Path databasePath = Paths.get(databaseName);
+        ;
+        try (Scanner readFile = new Scanner(databasePath)) {
+            while (readFile.hasNextLine()) {
+                System.out.println(readFile.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
 
     }
 }
